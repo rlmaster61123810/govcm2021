cef277c6df8478f5cedbd9c71832a9123468c8e6
The new begin <61123810@g.cmru.ac.th>
2021-11-30 14:20:38 +0700

    UIuser

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// group prefix admin
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+    Route::get('/', function () {
+        return view('admin.index');
+    });
+
+    Route::resource('/users', App\Http\Controllers\Admin\UserController::class);
+    Route::resource('/roles', App\Http\Controllers\Admin\RoleController::class);
+    Route::resource('/permissions', App\Http\Controllers\Admin\PermissionController::class);
 });
